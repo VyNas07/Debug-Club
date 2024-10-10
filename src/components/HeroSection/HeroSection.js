ed91eab5e433cae0b794c998adb385f8a8681c15
ArthurLima05 <arthurralima2005@gmail.com>
2024-10-09 23:48:39 -0300

    Estilização da Hero Section e Responsividade da Hero e do Header

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -5,12 +5,15 @@ import dashboard_img from '../../assets/painel-de-controle.png';
 const HeroSection = () => {
   return (
     <section className = "hero-section">
+      <div className='hero-text'>
       <h1>Resolva Bugs. Ganhe Pontos. Alcance o topo!</h1>
       <p>
       Contribua para projetos de código aberto, melhore suas habilidades e ganhe reconhecimento.
       Junte-se à comunidade e faça a diferença!
       </p>
       <button className='button-start'>Começar</button>
+      </div>
+    
       <img src= {dashboard_img} alt = 'imagem dashboard' className='dashboard-img'/>
     </section>
   );
