823f778a58292c4ff374d408ef0176fe6d932c37
ArthurLima05 <arthurralima2005@gmail.com>
2024-10-13 16:42:29 -0300

    Finalização da HomePage

--- src/components/HomePage/RankingSection/RankingSection.js
+++ src/components/HomePage/RankingSection/RankingSection.js
@@ -5,7 +5,9 @@ import RankingImg from '../../../assets/IMG-HomePage/Ranking.png'
 const RankingSection = () => {
   return (
     <section className = 'ranking-section'>
-        <h2>Ranking de Contribuições</h2>
+      <div className='ranking-text'>
+        <h2>Ranking <span>de</span> Contribuições</h2>
+        </div>
         <img src={RankingImg} alt = 'Imagem de Ranking' className='ranking-img'/>
     </section>
   );;
--- src/components/HomePage/Rodape/Rodape.js
+++ src/components/HomePage/Rodape/Rodape.js
@@ -2,29 +2,37 @@ import React from 'react';
 import logoDebugClub from '../../../assets/IMG-HomePage/DebugClub.svg';
 import facebookLogo from '../../../assets/IMG-HomePage/facebook.png';
 import instagramLogo from '../../../assets/IMG-HomePage/instagram.png';
+import './Rodape.css';
 
 
 const Rodape = () => {
   return (
     <footer className='rodape-section'>
-      <img src={logoDebugClub} alt='logo' className='logo-img'/>
-      <div className='contato-rodape'>
-        <h3>Contato</h3>
-        <p>+55 81 99999 9999</p>
-        <p>contato@debugclub.com</p>
-        <ul className='icons-rodape'>
-            <li>
-                <a href='/' aria-label='Facebook'><img src={facebookLogo} alt='Facebook'/></a>
-            </li>
-            <li>
-                <a href='/' aria-label = 'Instagram'><img src={instagramLogo} alt = 'Instagram'/></a>
-            </li>
-        </ul>
-      </div>
-      <div className='endereco-rodape'>
-        <p>Cais do Apolo, 463 - Bairro do Recife</p>
-        <p>Recife - Pernambuco</p>
-      </div>
+      <div className='rodape-content'>
+          <img src={logoDebugClub} alt='logo' className='logo-img'/>
+          <div className='contato-rodape'>
+            <h3>Contato</h3>
+            <p>+55 81 99999 9999</p>
+            <p>contato@debugclub.com</p>
+            <ul className='icons-rodape'>
+              <li>
+                <a href='/' aria-label='Facebook'>
+                  <img src={facebookLogo} alt='Facebook' className='facebook-img'/>
+                </a>
+              </li>
+              <li>
+                <a href='/' aria-label='Instagram'>
+                  <img src={instagramLogo} alt='Instagram' className='instagram-img'/>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='endereco-rodape'>
+          <p>Rua Aqui Proximo, 19 - Madalena</p>
+          <p>Recife - Pernambuco</p>
+        </div>
+    
     </footer>
   );
 };
