43c0e7527e8703ed743ba676aa3d3a4419792e9f
mwndrly <mwndrly@gmail.com>
2024-10-17 20:13:42 -0300

    fix: add app context to main registration

--- src/components/RegistrationPage/MainRegistration/MainRegistration.js
+++ src/components/RegistrationPage/MainRegistration/MainRegistration.js
@@ -1,41 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './MainRegistration.css';
 import {initializeApp} from 'firebase/app'
 import { Outlet, Link } from "react-router-dom";
 import logoGitHub from '../../../assets/IMG-RegistrationPage/githubazul.png';
 import App, { firebaseApp } from '../../../App';
+import { AppContext } from '../../../AppContext';
 
 const MainRegistration = () => {
 	let app = App();
+
+    const { email, setEmail, senha, setSenha, criarUser } = useContext(AppContext);
+
 	return (
-		<main className="flex">
-		<div className="form">
-				<div className='container-logo'><a href='/'><img src= {logoGitHub} alt = 'LogoGitHub' className='logoGitHub'></img></a></div>
-				<form action="/" method="post">
-						<div className="caixa">
-								<h1 className="textologin">Inscreva-se</h1>
+        <main className="flex">
+        <div className="form">
+          <div className='container-logo'>
+            <a href='/'><img src={logoGitHub} alt='LogoGitHub' className='logoGitHub'></img></a>
+          </div>
+          <form action="/" method="post">
+            <div className="caixa">
+              <h1 className="textologin">Inscreva-se</h1>
 
-								<div className="email">
-										<input type="email" placeholder="E-mail" value={app.email} onChange={e => app.setEmail(e.target.value)} required />
-								</div>
+              <div className="email">
+                <input type="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} required />
+              </div>
 
-								<div className="senha">
-										<input type="password" placeholder="Senha" value={app.senha} onChange={e => app.setSenha(e.target.value)} required />
-								</div>
+              <div className="senha">
+                <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} required />
+              </div>
 
-								<div className="confirmar-senha">
-										<input type="password" placeholder="Confirmar Senha"  required />
-								</div>
-						</div>
+              <div className="confirmar-senha">
+                <input type="password" placeholder="Confirmar Senha" required />
+              </div>
+            </div>
 
-						<div className="entrar">
-								<p>Já tem uma conta?
-										<Link to = '/login'><a href="/login/login.html" className="criar">Entrar</a></Link></p>
-								<input onClick={app.criarUser} type="submit" value="Cadastrar" />
-						</div>
-				</form>
-		</div>
-</main>
+            <div className="entrar">
+              <p>Já tem uma conta?
+                <Link to='/login'><a href="/login/login.html" className="criar">Entrar</a></Link></p>
+              <input onClick={criarUser} type="submit" value="Cadastrar" />
+            </div>
+          </form>
+        </div>
+      </main>
 	)
 }
 
