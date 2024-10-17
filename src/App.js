fca77260714048bd054341d635dccbbe3c29fa08
Joao Rietra <jhlrietra@gmail.com>
2024-10-17 18:34:18 -0300

    Bug fix trying?

--- src/App.js
+++ src/App.js
@@ -9,45 +9,49 @@ import {initializeApp} from 'firebase/app'
 import { collection, getFirestore, getDocs } from 'firebase/firestore';
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBF0rmKT8uJJLJ4_a0tJzGaKOG0hIYOvy8",
-  authDomain: "debug-club.firebaseapp.com",
-  projectId: "debug-club",
-  storageBucket: "debug-club.appspot.com",
-  messagingSenderId: "377935123161",
-  appId: "1:377935123161:web:2c93b065b1c45b1fc3a4c8",
-  measurementId: "G-N17LCLESNJ"
+	apiKey: "AIzaSyBF0rmKT8uJJLJ4_a0tJzGaKOG0hIYOvy8",
+	authDomain: "debug-club.firebaseapp.com",
+	projectId: "debug-club",
+	storageBucket: "debug-club.appspot.com",
+	messagingSenderId: "377935123161",
+	appId: "1:377935123161:web:2c93b065b1c45b1fc3a4c8",
+	measurementId: "G-N17LCLESNJ"
 });
 
 function App() {
-  const [senha, setSenha] = useState("");
-  const [email, setEmail] = useState("");
-  const [users, setUsers] = useState([]);
+	const [senha, setSenha] = useState("");
+	const [email, setEmail] = useState("");
+	const [users, setUsers] = useState([]);
 
-  const db = getFirestore(firebaseApp);
-  const userCollectionRef = collection(db, "users")
+	const db = getFirestore(firebaseApp);
+	const userCollectionRef = collection(db, "users");
 
-  function criarUser() {
-    console.log({email, senha});
-  }
 
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(userCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    }
-    getUsers();
+	function criarUser() {
+		console.log({email, senha});
+	}
 
-  },[])
+	useEffect(() => {
+		const getUsers = async () => {
+			const data = await getDocs(userCollectionRef);
+			setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		}
+		getUsers();
+	},[])
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/registration" element={<RegistrationPage />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/registration" element={<RegistrationPage />} />
+			</Routes>
+		</Router>
+	);
 }
 
+App.firebaseApp = firebaseApp;
+
+
 export default App;
+
--- src/components/RegistrationPage/MainRegistration/MainRegistration.js
+++ src/components/RegistrationPage/MainRegistration/MainRegistration.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import './MainRegistration.css';
+import {initializeApp} from 'firebase/app'
 import { Outlet, Link } from "react-router-dom";
 import logoGitHub from '../../../assets/IMG-RegistrationPage/githubazul.png';
-import App from '../../../App';
+import {App, firebaseApp} from '../../../App';
 
 const MainRegistration = () => {
-  return (
-    <main className="flex">
-    <div className="form">
-        <div className='container-logo'><a href='/'><img src= {logoGitHub} alt = 'LogoGitHub' className='logoGitHub'></img></a></div>
-        <form action="/" method="post">
-            <div className="caixa">
-                <h1 className="textologin">Inscreva-se</h1>
-                
-                <div className="email">
-                    <input type="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} required />
-                </div>
+	let app = App();
+	return (
+		<main className="flex">
+		<div className="form">
+				<div className='container-logo'><a href='/'><img src= {logoGitHub} alt = 'LogoGitHub' className='logoGitHub'></img></a></div>
+				<form action="/" method="post">
+						<div className="caixa">
+								<h1 className="textologin">Inscreva-se</h1>
 
-                <div className="senha">
-                    <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} required />
-                </div>
+								<div className="email">
+										<input type="email" placeholder="E-mail" value={app.email} onChange={e => app.setEmail(e.target.value)} required />
+								</div>
 
-                <div className="confirmar-senha">
-                    <input type="password" placeholder="Confirmar Senha"  required />
-                </div>
-            </div>
+								<div className="senha">
+										<input type="password" placeholder="Senha" value={app.senha} onChange={e => app.setSenha(e.target.value)} required />
+								</div>
 
-            <div className="entrar">
-                <p>Já tem uma conta? 
-                    <Link to = '/login'><a href="/login/login.html" className="criar">Entrar</a></Link></p>
-                <input onClick={criarUser} type="submit" value="Cadastrar" />
-            </div>
-        </form>
-    </div>
+								<div className="confirmar-senha">
+										<input type="password" placeholder="Confirmar Senha"  required />
+								</div>
+						</div>
+
+						<div className="entrar">
+								<p>Já tem uma conta?
+										<Link to = '/login'><a href="/login/login.html" className="criar">Entrar</a></Link></p>
+								<input onClick={app.criarUser} type="submit" value="Cadastrar" />
+						</div>
+				</form>
+		</div>
 </main>
-  )
+	)
 }
 
 export default MainRegistration
